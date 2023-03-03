ed6cef351667f8c6bd11c076e01cfd3d714f925b
zuhaibhashim313 <almoghalss@gmail.com>
2023-03-03 14:25:07 -0500

    changes made from theia lab environment

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -8,24 +8,26 @@ urlpatterns = [
     # route is a string contains a URL pattern
     # view refers to the view function
     # name the URL
+    path(route='', view=views.get_dealerships, name='index'),
 
-   path(route='about/', view=views.about, name='about'),
+    # path for about view
+    path(route='about/', view=views.about, name='about'),
 
     # path for contact us view
     path(route='contact/', view=views.contact, name='contact'),
 
-
     # path for registration
     path(route='registration/', view=views.registration_request, name='registration'),
+
     # path for login
     path(route='login/', view=views.login_request, name='login'),
 
     # path for logout
     path(route='logout/', view=views.logout_request, name='logout'),
-    path(route='', view=views.get_dealerships, name='index'),
 
     # path for dealer reviews view
+    path(route='dealer/<int:dealer_id>/', view=views.get_dealer_details, name='dealer_details'),
 
-    # path for add a review view
-
+    # path for adding a new review view
+    path(route='dealer/<int:dealer_id>/add-review/', view=views.add_review, name="add_review")
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -2,24 +2,32 @@ from django.shortcuts import render
 from django.http import HttpResponseRedirect, HttpResponse
 from django.contrib.auth.models import User
 from django.shortcuts import get_object_or_404, render, redirect
-# from .models import related models
-# from .restapis import related methods
+#from .models import CarModel
+from .restapis import get_dealer_by_id, get_dealers_from_cf, get_dealers_by_state, get_dealer_reviews_from_cf, post_request
 from django.contrib.auth import login, logout, authenticate
 from django.contrib import messages
 from datetime import datetime
 import logging
 import json
 
-# Get an instance of a logger
+# Getting an instance of a logger
 logger = logging.getLogger(__name__)
 
 
-# Create your views here.
+# View to render the index page with a list of dealerships
+def get_dealerships(request):
+    if request.method == "GET":
+        context = {}
+        url = "https://us-south.functions.appdomain.cloud/api/v1/web/17c71621-9fbd-4875-906b-2c38306fde85/dealership-package/get-dealerships"
+        # Get dealers from the Cloudant DB
+        context["dealerships"] = get_dealers_from_cf(url)
 
+        # dealer_names = ' '.join([dealer.short_name for dealer in context["dealerships"]])
+        # return HttpResponse(dealer_names)
 
-# Create an `about` view to render a static about page
-# def about(request):
-# ...
+        return render(request, 'djangoapp/index.html', context)
+
+# View to render a static about page
 def about(request):
     context = {}
     if request.method == "GET":
@@ -32,12 +40,8 @@ def contact(request):
     if request.method == "GET":
         return render(request, 'djangoapp/contact.html', context)
 
-# Create a `contact` view to return a static contact page
-#def contact(request):
 
-# Create a `login_request` view to handle sign in request
-# def login_request(request):
-# ...
+# View to handle sign in request
 def login_request(request):
     context = {}
     if request.method == "POST":
@@ -54,9 +58,7 @@ def login_request(request):
         return render(request, 'djangoapp/login.html', context)
 
 
-# Create a `logout_request` view to handle sign out request
-# def logout_request(request):
-# ...
+# View to handle sign out request
 def logout_request(request):
     # Get the user object based on session id in request
     print("Logging out `{}`...".format(request.user.username))
@@ -65,9 +67,8 @@ def logout_request(request):
     # Redirect user back to course list view
     return redirect('djangoapp:index')
 
-# Create a `registration_request` view to handle sign up request
-# def registration_request(request):
-# ...
+
+# View to handle sign up request
 def registration_request(request):
     context = {}
     # If it is a GET request, just render the registration page
@@ -99,18 +100,68 @@ def registration_request(request):
         else:
             return render(request, 'djangoapp/registration.html', context)
 
-# Update the `get_dealerships` view to render the index page with a list of dealerships
-def get_dealerships(request):
+
+# View to render the reviews of a dealer
+def get_dealer_details(request, dealer_id):
     context = {}
     if request.method == "GET":
-        return render(request, 'djangoapp/index.html', context)
+        url = 'https://us-south.functions.appdomain.cloud/api/v1/web/17c71621-9fbd-4875-906b-2c38306fde85/dealership-package/get-review'
+        reviews = get_dealer_reviews_from_cf(url, dealer_id=dealer_id)
+        context = {
+            "reviews":  reviews, 
+            "dealer_id": dealer_id
+        }
+
+        return render(request, 'djangoapp/dealer_details.html', context)
 
 
-# Create a `get_dealer_details` view to render the reviews of a dealer
-# def get_dealer_details(request, dealer_id):
-# ...
+# View to submit a new review
+def add_review(request, dealer_id):
+    # User must be logged in before posting a review
+    if request.user.is_authenticated:
+        # GET request renders the page with the form for filling out a review
+        if request.method == "GET":
+            url = f"https://us-south.functions.appdomain.cloud/api/v1/web/17c71621-9fbd-4875-906b-2c38306fde85/dealership-package/get-dealerships?dealerId={dealer_id}"
+            # Get dealer details from the API
+            context = {
+                "cars": CarModel.objects.all(),
+                "dealer": get_dealer_by_id(url, dealer_id=dealer_id),
+            }
+            return render(request, 'djangoapp/add_review.html', context)
 
-# Create a `add_review` view to submit a review
-# def add_review(request, dealer_id):
-# ...
+        # POST request posts the content in the review submission form to the Cloudant DB using the post_review Cloud Function
+        if request.method == "POST":
+            form = request.POST
+            review = dict()
+            review["name"] = f"{request.user.first_name} {request.user.last_name}"
+            review["dealership"] = dealer_id
+            review["review"] = form["content"]
+            review["purchase"] = form.get("purchasecheck")
+            if review["purchase"]:
+                review["purchase_date"] = datetime.strptime(form.get("purchasedate"), "%m/%d/%Y").isoformat()
+            car = CarModel.objects.get(pk=form["car"])
+            review["car_make"] = car.car_make.name
+            review["car_model"] = car.name
+            review["car_year"] = car.year
+            
+            # If the user bought the car, get the purchase date
+            if form.get("purchasecheck"):
+                review["purchase_date"] = datetime.strptime(form.get("purchasedate"), "%m/%d/%Y").isoformat()
+            else: 
+                review["purchase_date"] = None
 
+            url = "https://us-south.functions.appdomain.cloud/api/v1/web/17c71621-9fbd-4875-906b-2c38306fde85/dealership-package/get-review"  # API Cloud Function route
+            json_payload = {"review": review}  # Create a JSON payload that contains the review data
+
+            # Performing a POST request with the review
+            result = post_request(url, json_payload, dealerId=dealer_id)
+            if int(result.status_code) == 200:
+                print("Review posted successfully.")
+
+            # After posting the review the user is redirected back to the dealer details page
+            return redirect("djangoapp:dealer_details", dealer_id=dealer_id)
+
+    else:
+        # If user isn't logged in, redirect to login page
+        print("User must be authenticated before posting a review. Please log in.")
+        return redirect("/djangoapp/login")
